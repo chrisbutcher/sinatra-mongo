45a8be62c30db86a4bda164113a13e23cb7acbba
Chris Butcher <cbutcher@gmail.com>
2013-08-15 21:01:09 -0400

    Changed to mongo_mapper

--- app.rb
+++ app.rb
@@ -1,36 +1,27 @@
 # http://recipes.sinatrarb.com/p/databases/mongo?#article
+# http://128bitstudios.com/2012/03/03/mongomapper-with-sinatra-example/
+# http://mongomapper.com/documentation/getting-started/sinatra.html
 
 require 'rubygems'
 require 'sinatra'
-require 'mongo'
-require 'json/ext' # required for .to_json
-
-include Mongo
+require 'mongo_mapper'
 
 configure do
-  conn = MongoClient.new("localhost", 27017)
-  set :mongo_connection, conn
-  set :mongo_db, conn.db('books')
+  MongoMapper.database = 'books'
 end
 
-helpers do
-  def object_id val
-    BSON::ObjectId.from_string(val)
-  end
-
-  def document_by_id id
-    id = object_id(id) if String === id
-    settings.mongo_db['books'].find_one(:_id => id).to_json
-  end
+class Book
+  include MongoMapper::Document
+  key :title, String
+  key :author, String
 end
 
 get '/' do
   "Hello, world!"
 end
 
-get '/books/all' do
-  # content_type :json
-  @books = settings.mongo_db['books'].find({}, :sort => 'title')
+get '/books' do
+  @books = Book.all
   erb :books
 end
 
@@ -38,7 +29,9 @@ get '/books/add' do
   erb :add_book
 end
 
-post '/books/add' do
-  new_id = settings.mongo_db['books'].insert params
-  redirect '/books/all'
+post "/books/create" do
+  @new_book = Book.new(:title=>params[:title], :author=>params[:author])
+  @new_book.save
+
+  redirect "/books"
 end
